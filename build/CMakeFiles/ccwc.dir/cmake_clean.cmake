0bc5c26e06f54fa8aab629cd72977599cdb5a3a0
amar <amardinkarraikwade101@gmail.com>
2025-03-08 09:05:50 +0000

    initial commit

---
+++ build/CMakeFiles/ccwc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ccwc.dir/main.cpp.o"
+  "CMakeFiles/ccwc.dir/main.cpp.o.d"
+  "bin/ccwc"
+  "bin/ccwc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ccwc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
